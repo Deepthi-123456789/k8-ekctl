e3388e2a179ecf389df8c8c0d45922902c5ed1bf
Deepthi <jonnalagaddadeepthi96@gmail.com>
2024-10-19 13:28:30 +0530

    mesg

--- workstation.tf
+++ workstation.tf
@@ -4,8 +4,8 @@ module "ec2_instance" {
   name = "workstation for ekctl"
   ami = "ami-0b4f379183e5706b9"
   instance_type          = "t2.micro"
-  key_name               = "user1"
-  monitoring             = true
+  # key_name               = "user1"
+  # monitoring             = true
   vpc_security_group_ids = [aws_security_group.allow_ekctl.id]
   subnet_id              = "subnet-01c1c823852d19a68"
   user_data = file("workstation.sh")
